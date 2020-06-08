8a44172959e2a975952720e22a2500a868dfa3be
urvishmhjn <65328197+urvishmhjn@users.noreply.github.com>
2020-06-09 01:12:54 +0530

    Imported Modules

---
+++ hackathon.py
@@ -0,0 +1,8 @@
+%matplotlib notebook
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+plt.style.use('seaborn')
